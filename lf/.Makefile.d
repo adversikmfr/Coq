4253a3ff5ffe579bf951aeebed41193604220765
Enfield <16650383171@163.com>
2025-08-18 11:57:04 +0800

    2025_8_18

---
+++ lf/.Makefile.d
@@ -0,0 +1,80 @@
+AltAuto.vo AltAuto.glob AltAuto.v.beautified AltAuto.required_vo: AltAuto.v 
+AltAuto.vos AltAuto.vok AltAuto.required_vos: AltAuto.v 
+AltAutoTest.vo AltAutoTest.glob AltAutoTest.v.beautified AltAutoTest.required_vo: AltAutoTest.v 
+AltAutoTest.vos AltAutoTest.vok AltAutoTest.required_vos: AltAutoTest.v 
+Auto.vo Auto.glob Auto.v.beautified Auto.required_vo: Auto.v 
+Auto.vos Auto.vok Auto.required_vos: Auto.v 
+AutoTest.vo AutoTest.glob AutoTest.v.beautified AutoTest.required_vo: AutoTest.v 
+AutoTest.vos AutoTest.vok AutoTest.required_vos: AutoTest.v 
+Basics.vo Basics.glob Basics.v.beautified Basics.required_vo: Basics.v 
+Basics.vos Basics.vok Basics.required_vos: Basics.v 
+BasicsTest.vo BasicsTest.glob BasicsTest.v.beautified BasicsTest.required_vo: BasicsTest.v 
+BasicsTest.vos BasicsTest.vok BasicsTest.required_vos: BasicsTest.v 
+Bib.vo Bib.glob Bib.v.beautified Bib.required_vo: Bib.v 
+Bib.vos Bib.vok Bib.required_vos: Bib.v 
+BibTest.vo BibTest.glob BibTest.v.beautified BibTest.required_vo: BibTest.v 
+BibTest.vos BibTest.vok BibTest.required_vos: BibTest.v 
+Extraction.vo Extraction.glob Extraction.v.beautified Extraction.required_vo: Extraction.v 
+Extraction.vos Extraction.vok Extraction.required_vos: Extraction.v 
+ExtractionTest.vo ExtractionTest.glob ExtractionTest.v.beautified ExtractionTest.required_vo: ExtractionTest.v 
+ExtractionTest.vos ExtractionTest.vok ExtractionTest.required_vos: ExtractionTest.v 
+Imp.vo Imp.glob Imp.v.beautified Imp.required_vo: Imp.v 
+Imp.vos Imp.vok Imp.required_vos: Imp.v 
+ImpCEvalFun.vo ImpCEvalFun.glob ImpCEvalFun.v.beautified ImpCEvalFun.required_vo: ImpCEvalFun.v 
+ImpCEvalFun.vos ImpCEvalFun.vok ImpCEvalFun.required_vos: ImpCEvalFun.v 
+ImpCEvalFunTest.vo ImpCEvalFunTest.glob ImpCEvalFunTest.v.beautified ImpCEvalFunTest.required_vo: ImpCEvalFunTest.v 
+ImpCEvalFunTest.vos ImpCEvalFunTest.vok ImpCEvalFunTest.required_vos: ImpCEvalFunTest.v 
+ImpParser.vo ImpParser.glob ImpParser.v.beautified ImpParser.required_vo: ImpParser.v 
+ImpParser.vos ImpParser.vok ImpParser.required_vos: ImpParser.v 
+ImpParserTest.vo ImpParserTest.glob ImpParserTest.v.beautified ImpParserTest.required_vo: ImpParserTest.v 
+ImpParserTest.vos ImpParserTest.vok ImpParserTest.required_vos: ImpParserTest.v 
+ImpTest.vo ImpTest.glob ImpTest.v.beautified ImpTest.required_vo: ImpTest.v 
+ImpTest.vos ImpTest.vok ImpTest.required_vos: ImpTest.v 
+IndPrinciples.vo IndPrinciples.glob IndPrinciples.v.beautified IndPrinciples.required_vo: IndPrinciples.v 
+IndPrinciples.vos IndPrinciples.vok IndPrinciples.required_vos: IndPrinciples.v 
+IndPrinciplesTest.vo IndPrinciplesTest.glob IndPrinciplesTest.v.beautified IndPrinciplesTest.required_vo: IndPrinciplesTest.v 
+IndPrinciplesTest.vos IndPrinciplesTest.vok IndPrinciplesTest.required_vos: IndPrinciplesTest.v 
+IndProp.vo IndProp.glob IndProp.v.beautified IndProp.required_vo: IndProp.v 
+IndProp.vos IndProp.vok IndProp.required_vos: IndProp.v 
+IndPropTest.vo IndPropTest.glob IndPropTest.v.beautified IndPropTest.required_vo: IndPropTest.v 
+IndPropTest.vos IndPropTest.vok IndPropTest.required_vos: IndPropTest.v 
+Induction.vo Induction.glob Induction.v.beautified Induction.required_vo: Induction.v 
+Induction.vos Induction.vok Induction.required_vos: Induction.v 
+InductionTest.vo InductionTest.glob InductionTest.v.beautified InductionTest.required_vo: InductionTest.v 
+InductionTest.vos InductionTest.vok InductionTest.required_vos: InductionTest.v 
+Lists.vo Lists.glob Lists.v.beautified Lists.required_vo: Lists.v 
+Lists.vos Lists.vok Lists.required_vos: Lists.v 
+ListsTest.vo ListsTest.glob ListsTest.v.beautified ListsTest.required_vo: ListsTest.v 
+ListsTest.vos ListsTest.vok ListsTest.required_vos: ListsTest.v 
+Logic.vo Logic.glob Logic.v.beautified Logic.required_vo: Logic.v 
+Logic.vos Logic.vok Logic.required_vos: Logic.v 
+LogicTest.vo LogicTest.glob LogicTest.v.beautified LogicTest.required_vo: LogicTest.v 
+LogicTest.vos LogicTest.vok LogicTest.required_vos: LogicTest.v 
+Maps.vo Maps.glob Maps.v.beautified Maps.required_vo: Maps.v 
+Maps.vos Maps.vok Maps.required_vos: Maps.v 
+MapsTest.vo MapsTest.glob MapsTest.v.beautified MapsTest.required_vo: MapsTest.v 
+MapsTest.vos MapsTest.vok MapsTest.required_vos: MapsTest.v 
+Poly.vo Poly.glob Poly.v.beautified Poly.required_vo: Poly.v 
+Poly.vos Poly.vok Poly.required_vos: Poly.v 
+PolyTest.vo PolyTest.glob PolyTest.v.beautified PolyTest.required_vo: PolyTest.v 
+PolyTest.vos PolyTest.vok PolyTest.required_vos: PolyTest.v 
+Postscript.vo Postscript.glob Postscript.v.beautified Postscript.required_vo: Postscript.v 
+Postscript.vos Postscript.vok Postscript.required_vos: Postscript.v 
+PostscriptTest.vo PostscriptTest.glob PostscriptTest.v.beautified PostscriptTest.required_vo: PostscriptTest.v 
+PostscriptTest.vos PostscriptTest.vok PostscriptTest.required_vos: PostscriptTest.v 
+Preface.vo Preface.glob Preface.v.beautified Preface.required_vo: Preface.v 
+Preface.vos Preface.vok Preface.required_vos: Preface.v 
+PrefaceTest.vo PrefaceTest.glob PrefaceTest.v.beautified PrefaceTest.required_vo: PrefaceTest.v 
+PrefaceTest.vos PrefaceTest.vok PrefaceTest.required_vos: PrefaceTest.v 
+ProofObjects.vo ProofObjects.glob ProofObjects.v.beautified ProofObjects.required_vo: ProofObjects.v 
+ProofObjects.vos ProofObjects.vok ProofObjects.required_vos: ProofObjects.v 
+ProofObjectsTest.vo ProofObjectsTest.glob ProofObjectsTest.v.beautified ProofObjectsTest.required_vo: ProofObjectsTest.v 
+ProofObjectsTest.vos ProofObjectsTest.vok ProofObjectsTest.required_vos: ProofObjectsTest.v 
+Rel.vo Rel.glob Rel.v.beautified Rel.required_vo: Rel.v 
+Rel.vos Rel.vok Rel.required_vos: Rel.v 
+RelTest.vo RelTest.glob RelTest.v.beautified RelTest.required_vo: RelTest.v 
+RelTest.vos RelTest.vok RelTest.required_vos: RelTest.v 
+Tactics.vo Tactics.glob Tactics.v.beautified Tactics.required_vo: Tactics.v 
+Tactics.vos Tactics.vok Tactics.required_vos: Tactics.v 
+TacticsTest.vo TacticsTest.glob TacticsTest.v.beautified TacticsTest.required_vo: TacticsTest.v 
+TacticsTest.vos TacticsTest.vok TacticsTest.required_vos: TacticsTest.v 
